c93851967f7b30a0808a0683e9c65c6c794300e1
bojan1995 <37152424+bojan1995@users.noreply.github.com>
2020-04-12 15:53:28 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -1,13 +1,48 @@
 class Point {
 	String id;
 	String color;
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
 	double x,y;
-	
+
 	//TODO add new variable
 
 	//TODO constructor
 
 	//TODO setters and getters
+	double getAverage() {
+		
+	}
 
 	public void move (char xDirection, char yDirection) {
 		//TODO
